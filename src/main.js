5423a5564ab8b6c70d4186cf1fb0cf3b5666a390
yura <yura.zb.ua@gmail.com>
2022-11-02 10:23:23 +0200

    fixed bug

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import BootstrapVue3 from 'bootstrap-vue-3'
 import store from "./store";
-import VueGoogleMaps from '@fawmi/vue-google-maps'
-
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
