838780a55518caf32948b11c2943f6ff8aba2d70
yura <yura.zb.ua@gmail.com>
2022-11-03 17:20:23 +0200

    Code refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
   import Navbar from "@/components/Navbar";
   import store from '@/store/index.js';
-
 </script>
 
 <template>
@@ -12,19 +11,10 @@
     <main class="content">
       <RouterView />
     </main>
-    <footer class="footer d-flex flex-wrap justify-content-between align-items-center py-3 my-1 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-        </a>
+    <footer class="footer py-3 my-1 border-top">
+      <div class="text-center">
         <span class="mb-3 mb-md-0 text-muted">© {{getCurrentYear()}} Dekhtiar, Inc</span>
       </div>
-
-      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-      </ul>
     </footer>
   </div>
 </template>
