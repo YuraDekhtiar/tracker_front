ebcc58d47a832ddba1f1a65684eaa6d08c0ad7d1
yura <yura.zb.ua@gmail.com>
2022-11-03 14:09:13 +0200

    fixed bug

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row justify-content-center">
+  <div class="container">
     <div class="col-md-12 col-sm-12">
       <div class="card shadow-lg border-0 rounded-lg mt-5 mx-auto" style="width: 30rem;">
         <h3 class="card-header display-1 text-muted text-center">
