471650750bad4adc2ed44ebbe23aef03dbc94140
yura <yura.zb.ua@gmail.com>
2023-01-19 11:01:28 +0200

    init new table

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -5,40 +5,51 @@
 
 <template>
   <vPreloader v-if="isLoading"/>
-
   <div v-else class="container">
-      <h2 class="text-center">Users</h2>
-      <RouterLink class="float-end btn btn-info" :to="`/create-new-user`">Create user</RouterLink>
-      <div v-if="errorResponse" class="alert alert-danger" role="alert">
+    <RouterLink class="float-end btn btn-info" :to="`/create-new-user`">Create user</RouterLink>
+    <h2 class="text-center">Users</h2>
+    <div v-if="errorResponse" class="alert alert-danger" role="alert">
         {{errorMessage}}
       </div>
       <div v-else>
-        <table class="table">
-          <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Username</th>
-            <th scope="col">Email</th>
-            <th scope="col">Last visit</th>
-            <th scope="col">Roles</th>
-            <th scope="col">Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="(item, i) in users" v-bind:key="i">
-            <th scope="row">{{ i+1 }}</th>
-            <td>{{ item.username }}</td>
-            <td>{{ item.email }}</td>
-            <td>{{ dateFilter(item.last_visit) }}</td>
-            <td>
-              <div v-for="(item, i) in item.roles" v-bind:key="i">
-                <div>{{item.name}}</div>
-              </div>
-            </td>
-            <td><button class="btn btn-danger" title="Delete" @click="deleteHandler(item.id)">X</button></td>
-          </tr>
-          </tbody>
-        </table>
+        <vue-good-table
+          compactMode
+          :columns="columns"
+          :rows="users"
+          :line-numbers="true"
+          :select-options="{
+            enabled: true,
+            selectionInfoClass: 'selection-info',
+            selectOnCheckboxOnly: true
+          }"
+          :search-options="{
+            placeholder: 'Search',
+            enabled: true,
+          }"
+          :sort-options="{
+            enabled: true,
+            initialSortBy: {field: 'username', type: 'asc'}
+          }"
+      >
+        <template #table-row="props">
+          <span v-if="props.column.field === 'actions'" >
+            <button class="btn p-0 me-2" title="Delete" @click="deleteHandler(props.row.id)">
+                <BootstrapIcon
+                  icon="trash3-fill"
+                  size="2x"
+                  color="red"
+                />
+              </button>
+          </span>
+          <span v-else-if="props.column.field === 'roles'">
+            {{props.row.roles[0].name}}
+          </span>
+          <span v-else-if="props.column.field === 'last_visit'">
+            {{ dateFilter(props.row.last_visit) }}
+          </span>
+        </template>
+      </vue-good-table>
+
       </div>
     </div>
 </template>
@@ -53,7 +64,42 @@ export default {
       isLoading: Boolean,
       users: Array,
       errorMessage: "",
-      errorResponse: false
+      errorResponse: false,
+      columns: [
+        {
+          label: 'Username',
+          field: 'username',
+          type: 'string',
+          tdClass: 'text-start',
+        },
+        {
+          label: 'Email',
+          field: 'email',
+          type: 'string',
+          tdClass: 'text-start',
+        },
+        {
+          label: 'Last visit',
+          field: 'last_visit',
+          type: 'date',
+          dateInputFormat: 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'',
+          dateOutputFormat: 'dd/MM/yyyy HH:mm:ss',
+          tdClass: 'text-start',
+        },
+        {
+          label: 'Roles',
+          field: 'roles',
+          type: 'string',
+          tdClass: 'text-start',
+        },
+        {
+          label: 'Actions',
+          field: 'actions',
+          sortable: false,
+          tdClass: 'text-center',
+        }
+      ],
+
     }
   },
   computed: {
