a48b6dc5327f8cb2088ae54c1bbd1e5f3b3cb3e7
yura <yura.zb.ua@gmail.com>
2023-04-25 09:56:29 +0300

    code refactoring

--- src/commons/date.filter.js
+++ src/commons/date.filter.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
+import utc  from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 export default date => {
-
-  return dayjs(date).format('DD/MM/YYYY HH:mm:ss ');
+  return dayjs(date).format('DD/MM/YYYY HH:mm:ss');
 };
