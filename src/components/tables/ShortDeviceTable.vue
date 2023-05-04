2a543f7d469b8188ee452368cbcf75fffe326ec3
yura <yura.zb.ua@gmail.com>
2023-05-04 15:18:20 +0300

    decomposition to component

---
+++ src/components/tables/ShortDeviceTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <vue-good-table
+      compactMode
+      :columns="columns"
+      :rows="rows"
+      :line-numbers="true"
+      :search-options="{
+              placeholder: 'Search',
+              enabled: true,
+            }"
+      :sort-options="{
+              enabled: true,
+              initialSortBy: {field: 'name', type: 'asc'}
+            }"
+    >
+      <template #selected-row-actions>
+                <BootstrapIcon
+                  icon="trash3-fill"
+                  size="2x"
+                  color="red"
+                />
+      </template>
+      <template #table-row="props">
+            <span v-if="props.column.field === 'actions'">
+              <BootstrapIcon
+                    icon="trash3-fill"
+                    size="2x"
+                    color="red"
+                  />
+            </span>
+      </template>
+    </vue-good-table>
+  </div>
+</template>
+
+<script>
+
+import VDeleteButton from "@/components/buttons/DeleteButton.vue";
+import VCancelButton from "@/components/buttons/CancelButton.vue";
+
+export default {
+  name: "vShortDeviceTable",
+
+  data() {
+    return {
+      columns: [
+        {
+          label: 'Name',
+          field: 'name',
+          tdClass: 'text-start',
+        },
+        {
+          label: 'Actions',
+          field: 'actions',
+          width: '100px',
+          sortable: false,
+          tdClass: 'text-center',
+        },
+      ],
+    }
+  },
+  props: {
+    rows: Object
+  }
+}
+</script>
