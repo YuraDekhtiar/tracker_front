b719ab87dd261003c485cf7f026f771539d4f3c6
yura <yura.zb.ua@gmail.com>
2023-05-04 15:15:50 +0300

    init button

---
+++ src/components/buttons/SuccessButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-button variant="outline-success" @click="onClick">
+    {{ text }}
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: "vSuccessButton",
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  },
+  props: {
+    text: String,
+  },
+  emits: {
+    click: null
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
