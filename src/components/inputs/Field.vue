fbe4d5f1581dd317d08cab54bd8e9f2915925c97
Yura <yura.zb.ua@gmail.com>
2024-07-29 12:02:34 +0300

    added wrapper on Field

---
+++ src/components/inputs/Field.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Field
+      class="form-control mt-2"
+      :placeholder="placeholder"
+      :name="name"
+      :type="type"
+      :validateOnBlur="validateOnBlur"
+      v-model="input"
+      :onblur="inputUpdate"
+      trim
+    />
+  </div>
+</template>
+
+<script>
+
+
+
+import {Field} from "vee-validate";
+
+export default {
+  name: "vField",
+  components: {Field},
+  data() {
+    return {
+      input: "",
+    }
+  },
+  methods: {
+    inputUpdate() {
+      this.$emit('update:input', this.input)
+    }
+  },
+  props: {
+    placeholder: "",
+    name: "",
+    validateOnBlur: false,
+    type: "text",
+  },
+  emits: ['update:input'],
+}
+</script>
+
+<style scoped>
+
+</style>
