e919ef3a6d4038a3012dcead7712846a216e94e1
Yura <yura.zb.ua@gmail.com>
2024-07-29 12:02:25 +0300

    added wrapper on ErrorMessage

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import {ErrorMessage} from "vee-validate";
+</script>
+
+<template>
+  <ErrorMessage class="text-danger ms-2 mt-1" :name="name" as="div" />
+</template>
+
+<script>
+export default {
+  name: 'vErrorMessage',
+
+  props: {
+    name: ""
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
